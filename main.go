36de2a77b2bc9d3dbc220b412eb119ec1975f48b
wthme <wthme@sibmail.com>
2025-04-08 15:12:01 +0300

    docker rabotaet

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	handler "Go_wallet/handlers"
+	"Go_wallet/handlers"
 	"Go_wallet/pglogic"
 	"context"
 	"log"
@@ -13,40 +13,30 @@ import (
 )
 
 func main() {
-	// Load environment variables
 
-	if err := godotenv.Load("config.env"); err != nil {
+	if err := godotenv.Load(".env"); err != nil {
 		log.Fatalf("Error loading config.env file: %v", err)
 	}
 
 
-	dbPool, err := pgxpool.New(context.Background(), getDSN())
 
+	dbPool, err := pgxpool.New(context.Background(), "postgresql://postgres:12345@valet:5432/postgres")
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	defer dbPool.Close()
 
-
 	err = pglogic.InitDB(dbPool)
-
-
-
 	if err!= nil{
 		log.Fatalf("Can`t get table: %v\n", err)
 	}
-
 	log.Println("Wallets table ready")
 
 
-	// randuuid := "80ac1149-9777-4e18-889d-0372cf95019e"
-	// dbPool.Exec(context.Background() , "UPDATE wallets SET balance = 34568 WHERE id = $1" , randuuid)
 
 
 	walletdb := pglogic.NewWalletdb(dbPool)
-	walletHandler := handler.NewWalletHandler(*walletdb)
-
-
+	walletHandler := handlers.NewWalletHandler(*walletdb)
 	router := gin.Default()
 
 	api := router.Group("/api/v1")
@@ -55,7 +45,8 @@ func main() {
 		api.GET("/wallets/:walletId", walletHandler.GetWalletBalance)
 	}
 
-	err = router.Run(":" + os.Getenv("SERVER_PORT")) ; if err != nil{
+	err = router.Run(":" + os.Getenv("SERVER_PORT"))
+	if err != nil {
 		log.Fatalf("Problem with starting server %v", err)
 	}
 
